3492982dce774b76a0de3beda3880362c75d82b1
Anthony <anthonyb.ft@gmail.com>
2021-08-21 04:22:32 -0500

    new portfolio for 2021

---
+++ src/components/Template/SideBar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ContactIcons from '../Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const SideBar = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Anthony Buncio</h2>
+        <p>
+          <a href="mailto:anthonybuncio.work@gmail.com">
+            anthonybuncio.work@gmail.com
+          </a>
+        </p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>
+        Hi, I&apos;m Anthony. I like building things. I am a{' '}
+        <a href="https://www.hccs.edu/">HCC</a> student,{' '}
+        <a href="https://www.coursereport.com/schools/the-iron-yard">
+          Iron Yard
+        </a>{' '}
+        Alumni, and a self-taught freelance developer. Before Arthena I was at{' '}
+        <a href="https://matroid.com">Matroid</a>,{' '}
+        <a href="https://planet.com">Planet</a>,{' '}
+        <a href="https://planetaryresources.com">Planetary Resources</a>,{' '}
+        <a href="https://facebook.com">Facebook</a>, and{' '}
+        <a href="https://seds.org">SEDS</a>.
+      </p>
+      <ul className="actions">
+        <li>
+          {!window.location.pathname.includes('/resume') ? (
+            <Link to="/resume" className="button">
+              Learn More
+            </Link>
+          ) : (
+            <Link to="/about" className="button">
+              About Me
+            </Link>
+          )}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ContactIcons />
+      <p className="copyright">
+        &copy; Anthony Buncio <Link to="/">anthonybuncio.com</Link>.
+      </p>
+    </section>
+  </section>
+);
+
+export default SideBar;
---
+++ src/data/contact.js
@@ -0,0 +1,29 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+
+const data = [
+  {
+    link: 'https://github.com/AnthonyBuncio',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://linkedin.com/in/anthonybuncio1/',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://angel.co/u/anthony-buncio-1',
+    label: 'Angel List',
+    icon: faAngellist,
+  },
+  {
+    link: 'mailto:anthonybuncio.work@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
+];
+
+export default data;
